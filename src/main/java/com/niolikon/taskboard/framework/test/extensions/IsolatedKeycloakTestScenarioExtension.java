8f47a19490b6a3b488643b432a5f4f6b65c3de5d
Simone Andrea Muscas <simoneandrea.muscas@gmail.com>
2025-03-25 05:38:16 +0100

    Improve Keycloak Test Scenario Management
      TBF1

--- src/main/java/com/niolikon/taskboard/framework/test/extensions/IsolatedKeycloakTestScenarioExtension.java
+++ src/main/java/com/niolikon/taskboard/framework/test/extensions/IsolatedKeycloakTestScenarioExtension.java
@@ -1,9 +1,12 @@
 package com.niolikon.taskboard.framework.test.extensions;
 
+import com.niolikon.taskboard.framework.security.keycloak.KeycloakProperties;
 import com.niolikon.taskboard.framework.test.annotations.WithIsolatedKeycloakTestScenario;
+import dasniko.testcontainers.keycloak.KeycloakContainer;
 import org.junit.jupiter.api.extension.*;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.representations.idm.RealmRepresentation;
+import org.springframework.beans.BeansException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.lang.reflect.Method;
@@ -12,9 +15,27 @@ public class IsolatedKeycloakTestScenarioExtension implements BeforeEachCallback
 
     private Keycloak keycloakAdmin;
 
+    private KeycloakProperties keycloakProperties;
+    private String beanAuthServerUrl;
+    private String beanRealm;
+    private String beanClientId;
+    private String beanClientSecret;
+
     @Override
     public void beforeEach(ExtensionContext context) {
+        KeycloakContainer keycloakContainer = SpringExtension.getApplicationContext(context).getBean(KeycloakContainer.class);
         keycloakAdmin = SpringExtension.getApplicationContext(context).getBean(Keycloak.class);
+
+        try {
+            keycloakProperties = SpringExtension.getApplicationContext(context).getBean(KeycloakProperties.class);
+            beanAuthServerUrl = keycloakProperties.getAuthServerUrl();
+            beanRealm = keycloakProperties.getRealm();
+            beanClientId = keycloakProperties.getClientId();
+            beanClientSecret = keycloakProperties.getClientSecret();
+        }
+        catch (BeansException beansException) {
+            keycloakProperties = null;
+        }
 
         context.getTestMethod().ifPresent(method -> {
             WithIsolatedKeycloakTestScenario annotation = method.getAnnotation(WithIsolatedKeycloakTestScenario.class);
@@ -26,6 +47,16 @@ public class IsolatedKeycloakTestScenarioExtension implements BeforeEachCallback
                     Method datasetMethod = dataClass.getDeclaredMethod(methodName);
                     RealmRepresentation realm = (RealmRepresentation) datasetMethod.invoke(null);
 
+                    if (keycloakProperties != null) {
+                        if (! realm.getClients().isEmpty()) {
+                            keycloakProperties.setClientId(realm.getClients().get(0).getClientId());
+                            keycloakProperties.setClientSecret(realm.getClients().get(0).getSecret());
+                        }
+
+                        keycloakProperties.setAuthServerUrl(keycloakContainer.getAuthServerUrl());
+                        keycloakProperties.setRealm(realm.getRealm());
+                    }
+
                     keycloakAdmin.realms().create(realm);
                 } catch (Exception e) {
                     throw new RuntimeException("Error loading Keycloak realm for test: " + method.getName(), e);
@@ -36,6 +67,13 @@ public class IsolatedKeycloakTestScenarioExtension implements BeforeEachCallback
 
     @Override
     public void afterEach(ExtensionContext context) {
+        if (keycloakProperties != null) {
+            keycloakProperties.setAuthServerUrl(beanAuthServerUrl);
+            keycloakProperties.setRealm(beanRealm);
+            keycloakProperties.setClientId(beanClientId);
+            keycloakProperties.setClientSecret(beanClientSecret);
+        }
+
         if (keycloakAdmin == null) {
             throw new IllegalStateException("KeycloakAdminClient was not initialized!");
         }
